d90e73d7c967ff067901b9c2c2c87ad8b2a9a847
Swagat003 <109663081+Swagat003@users.noreply.github.com>
2024-06-23 00:21:07 +0530

    chore: Add favicon and update UI in Weather App

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import usePlaceWeather from './hooks/usePlaceWeather'
 function App() {
 
   const [city, setCity] = useState('')
+  const [region, setRegion] = useState('')
+  const [country, setCountry] = useState('')
   const [serCity, setSerCity] = useState('Bhubaneswar')
   const [temp, setTemp] = useState('')
   const [condition, setCondition] = useState('')
@@ -21,6 +23,8 @@ function App() {
   useEffect(() => {
     if (data && data.current) {
       setCity(data.location.name);
+      setRegion(data.location.region);
+      setCountry(data.location.country);
       setTemp(data.current.temp_c);
       setCondition(data.current.condition.text);
       setImg(data.current.condition.icon);
@@ -32,27 +36,28 @@ function App() {
     <>
       <div className="min-h-screen flex items-center justify-center bg-blue-50 p-4">
         <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-xs">
-          <div className="mb-4">
+          <div className="mb-4 flex gap-2">
             <input
               ref={inputSearch}
               type="text"
               placeholder="Search city"
-              className="w-full p-2 border rounded focus:outline-none focus:ring"
+              className="w-full p-2 border outline-none rounded-full"
             />
             <button
               onClick={handleSearch}
-              className="mt-2 w-full p-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+              className="  p-2 bg-blue-500 text-white rounded-full hover:bg-blue-600"
             >
               Search
             </button>
           </div>
           <div className="text-center mb-4">
-            <h2 className="text-xl font-semibold">
+            <h1 className="text-xl font-semibold">
               {city}
-            </h2>
+            </h1>
+            <p className='text-gray-800'>{region}, {country} </p>
           </div>
           <div className="flex items-center justify-between mb-4">
-            <div className="text-center">
+            <div>
               <div className="text-4xl">
                 {temp}°c
               </div>
@@ -63,7 +68,7 @@ function App() {
             <div>
               <img
                 src={img}
-                alt="Weather Icon" className="w-16 h-16" />
+                alt="Weather Icon" className="w-20" />
             </div>
           </div>
         </div>
